3eec80a6512854aa9a0145dc06636d234f83d8da
Lucas Gabriel <gabriel.lisboa.141@gmail.com>
2022-03-14 15:46:18 -0300

    verbo POST

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ server.get('/device', (req, res) => {
     res.send("Hello World");
 })
 
+server.post('/device', function (req, res) {
+    res.send('Got a POST request');
+});
+
 server.listen (port, () => {
     console.log(`Example server listening on port ${port}`)
 })
