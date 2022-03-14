aa68f0d048a708fbb2a681008e151c40b872a8ea
Lucas Gabriel <gabriel.lisboa.141@gmail.com>
2022-03-14 15:50:11 -0300

    verbo PUT

--- src/server.js
+++ src/server.js
@@ -4,12 +4,16 @@ const port = 3000;
 
 server.get('/device', (req, res) => {
     res.send("Hello World");
-})
+});
 
 server.post('/device', function (req, res) {
     res.send('Got a POST request');
 });
 
+server.put('/device', function (req, res) {
+    res.send('Got a PUT request');
+});
+
 server.listen (port, () => {
     console.log(`Example server listening on port ${port}`)
-})
+});
